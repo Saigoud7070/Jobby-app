85ac9b69de0928e7bfd492e9ca3771f118fb6691
sai <saikiran.panugantigreat@gmail.com>
2023-11-15 04:36:15 +0000

    not to get output in localhost

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Component from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import Login from './components/Login'
+import Home from './components/Home'
+
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect to="not-found" />
+      </Switch>
+    )
+  }
+}
+
+export default App
